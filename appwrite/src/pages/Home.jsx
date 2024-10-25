b14979f50af5fd7bc26663e16e55dba790bc792e
Srija Adhya <113666002+SrijaAdhya12@users.noreply.github.com>
2024-10-25 22:57:05 +0530

    feat: added user's name

--- appwrite/src/pages/Home.jsx
+++ appwrite/src/pages/Home.jsx
@@ -1,25 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { account, ID } from '../lib/appwrite'
+import { account } from '../lib/appwrite'
 import { Button } from '../components/ui/button'
-
 const Home = () => {
 	const navigate = useNavigate()
+	const [currentUser, setCurrentUser] = useState(null)
+
+	useEffect(() => {
+		const fetchUser = async () => {
+			try {
+				const user = await account.get()
+				setCurrentUser(user)
+			} catch (error) {
+				console.error('Error fetching user:', error)
+				navigate('/login')
+			}
+		}
+		fetchUser()
+	}, [navigate])
 
 	const logout = async () => {
-	    try {
-	        await account.deleteSession('current');
-	        navigate('/login');
-	    } catch (error) {
-	        console.error('Logout error:', error);
-	        if (error.code === 401) {
-	            console.log('Session already invalid. Redirecting to login.');
-	            navigate('/login');
-	        } else {
-	            console.error('Unexpected error during logout:', error);
-	        }
-	    }
-	};
+		try {
+			await account.deleteSession('current')
+			navigate('/login')
+		} catch (error) {
+			console.error('Logout error:', error)
+			if (error.code === 401) {
+				console.log('Session already invalid. Redirecting to login.')
+				navigate('/login')
+			} else {
+				console.error('Unexpected error during logout:', error)
+			}
+		}
+	}
 
 	return (
 		<div>
@@ -27,10 +40,9 @@ const Home = () => {
 			<Button variant="outline" onClick={logout}>
 				Logout
 			</Button>
+			<h1>Welcome {currentUser?.name}</h1>
 		</div>
 	)
 }
 
-export default Home;
-
-
+export default Home
